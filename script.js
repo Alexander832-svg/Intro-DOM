d329cd06c3856f169a0099e8351a87d82be5ac67
Alexander832-svg <chandervaldez@gmail.com>
2026-03-03 18:37:05 +0000

    progress

--- script.js
+++ script.js
@@ -3,19 +3,41 @@ console.log("Script Started");
 //Variable to track the number of clicks
 let clicks = 0;
 
+let clickValue = 1;
+
+//Variable to track the number of levels
+let level = 1;
+
 //Variable to store the click display h1 element
 let clickDisplay = document.getElementById("click-display");
 
+//Variable to store the level display h1 element
+let levelDisplay = document.getElementById("level-display");
+
 //Set the initial text for click count h1
 clickDisplay.innerText = "Clicks: " + clicks;
+
+//Set the initial text for level 
+levelDisplay.innerText = "Level: " + level;
 
 //Called each time the image is clicked
 function handleClick() {
     console.log("click");
 
     // Add one to click count
-    clicks = clicks + 1;
+    clicks = clicks + clickValue;
 
     // Update display
     clickDisplay.innerText = "Clicks: " + clicks;
+
+    //Check if have 10 cliks, ande if is level one or two
+    if (clicks == 10) {
+        alert("pop-up alert!");
+    }
+    else if (clicks >=9) {
+        levelDisplay.innerText = "Level: 2";
+    }
+    else if (clicks >= 10) {
+        clickValue = 2;
+    }
 }
